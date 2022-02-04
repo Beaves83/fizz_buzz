b8463080e6e6fc8a92daa89e37596bf899beec63
Bibi <bruiz@hispatec.com>
2022-02-05 00:28:14 +0100

    Begining the project

---
+++ src/components/Instructions.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="instructions">
+
+        <p> Fizz buzz is a group word game for children to teach
+            them about division. Players take turns to count
+            incrementally, replacing any number divisible by
+            three with the word "fizz", and any number divisible
+            by five with the word "buzz".</p>
+        <h3>Example: </h3>
+        <p> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz,
+            13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22,
+            23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32,
+            Fizz, 34, Buzz, Fizz, ...</p>
+        <h3>Note: </h3>
+        <p> Players taking turns is not applicable. A simple for
+            loop from 1 to 100 is sufficient</p>
+    </div>
+</template>
---
+++ src/views/Information.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="information">
+    <img alt="Info image" src="../assets/information.png">
+    <h1>This is the information about de game</h1>
+    <Instructions />
+  </div>
+</template>
+<script>
+import Instructions from '@/components/Instructions.vue';
+
+export default {
+  name: 'Information',
+  components: {
+    Instructions,
+  },
+};
+</script>
